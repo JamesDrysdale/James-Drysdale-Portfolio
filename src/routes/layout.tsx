563403e10cbd0544b619d0888c597180a8672680
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2023-05-26 18:16:36 +0100

    :goal_net: cleaned up more starter code from scaffold to leave a blank project ready to start building

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,9 +1,6 @@
 import { component$, Slot, useStyles$ } from '@builder.io/qwik';
 import { routeLoader$ } from '@builder.io/qwik-city';
 
-import Header from '~/components/starter/header/header';
-import Footer from '~/components/starter/footer/footer';
-
 import styles from './styles.css?inline';
 
 export const useServerTimeLoader = routeLoader$(() => {
@@ -16,11 +13,11 @@ export default component$(() => {
   useStyles$(styles);
   return (
     <>
-      <Header />
+      
       <main>
         <Slot />
       </main>
-      <Footer />
+      
     </>
   );
 });
