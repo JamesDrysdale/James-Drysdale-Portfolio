0b7c6fe270c835fe5c7e661437461119cc96301e
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2023-05-29 00:52:19 +0100

    Created about section with basic personal/work/educational details

--- src/root.tsx
+++ src/root.tsx
@@ -2,7 +2,7 @@ import { component$ } from '@builder.io/qwik';
 import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
 import { RouterHead } from './components/router-head/router-head';
 
-import './global.css';
+import './global.scss';
 
 export default component$(() => {
   /**
