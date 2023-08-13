6e1c0060260c3b4ee1b2eab503a3b98f978a3fce
JamesDrysdale <jamesdrysdale84@gmail.com>
2023-08-13 23:41:02 +0100

    Set up linting for the project to enhance code quality and consistency

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,44 +1,54 @@
 'use client';
 
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 const Header = () => {
-    const pathname = usePathname();
+  const pathname = usePathname();
 
-    const routes = [
-        { path: '/', label: 'Home' },
-        { path: '/about', label: 'About' },
-        { path: '/articles', label: 'Articles' },
-        { path: '/services', label: 'Services' },
-        { path: '/connect', label: 'Connect' },
-    ];
-    
-    return (
-        <header className="bg-slate-800 px-5 ">
-            <div className="flex prose prose-xl mx-auto justify-between place-items-center">
-                <div className="">
-                    <a href="#">
-                        <Image src="/images/logo.png" alt="James Drysdale Brand Logo" height={68} width={68}/>
-                    </a>
-                </div>
-                <section className="flex">
-                    <nav className="flex">
-                        {routes.map((route) => (
-                            <Link href={route.path} key={route.label} className='h-full'>
-                                <p className={`mx-2 text-lg flex items-center ${pathname === route.path && 'text-primary'}`}>{route.label}</p>
-                            </Link>
-                        ))}
-                    </nav>
-                    <div className='mx-3.5 items-center flex justify-start'>
-                        *
-                        {/* <DarkLightTheme /> */}
-                    </div>
-                </section>
-            </div>
-        </header>
-    )
+  const routes = [
+    { path: '/', label: 'Home' },
+    { path: '/about', label: 'About' },
+    { path: '/articles', label: 'Articles' },
+    { path: '/services', label: 'Services' },
+    { path: '/connect', label: 'Connect' },
+  ];
+
+  return (
+    <header className='bg-slate-800 px-5 '>
+      <div className='prose prose-xl mx-auto flex place-items-center justify-between'>
+        <div className=''>
+          <a href='#'>
+            <Image
+              src='/images/logo.png'
+              alt='James Drysdale Brand Logo'
+              height={68}
+              width={68}
+            />
+          </a>
+        </div>
+        <section className='flex'>
+          <nav className='flex'>
+            {routes.map((route) => (
+              <Link href={route.path} key={route.label} className='h-full'>
+                <p
+                  className={`mx-2 flex items-center text-lg ${
+                    pathname === route.path && 'text-primary'
+                  }`}
+                >
+                  {route.label}
+                </p>
+              </Link>
+            ))}
+          </nav>
+          <div className='mx-3.5 flex items-center justify-start'>
+            *{/* <DarkLightTheme /> */}
+          </div>
+        </section>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
