ba21b4a21c3c1390a37875abfefaf5c43019444c
JamesDrysdale <jamesdrysdale84@gmail.com>
2023-08-14 04:33:00 +0100

    Pulling articles through from posts directory for blog

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,7 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+
+const getPostMetadata = () => {
+  const folder = path.join(process.cwd(), 'app', 'posts');
+  const files = fs.readdirSync(folder);
+  const markdownPosts = files.filter((file) => file.endsWith('.md'));
+  const slugs = markdownPosts.map((file) => file.replace('.md', ''));
+  return slugs;
+};
+
 const Articles = () => {
-  return (
-    <h1>Articles</h1>
-  )
+  const postMetadata = getPostMetadata();
+  const postPreviews = postMetadata.map((slug) => (
+    <div key={slug}>
+      <h2>{slug}</h2>
+    </div>
+  ));
+
+  return <div>{postPreviews}</div>;
 };
 
 export default Articles;
