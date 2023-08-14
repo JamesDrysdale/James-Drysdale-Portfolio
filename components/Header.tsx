c1827f28aed10c7baaa932c0c3c5267b6f197097
JamesDrysdale <jamesdrysdale84@gmail.com>
2023-08-14 02:31:36 +0100

    Made header and navigation mobile responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,11 +3,13 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useTheme } from 'next-themes';
 
 import DarkLightTheme from './DarkLightTheme';
 
 const Header = () => {
   const pathname = usePathname();
+  const { theme } = useTheme();
 
   const routes = [
     { path: '/', label: 'Home' },
@@ -18,12 +20,16 @@ const Header = () => {
   ];
 
   return (
-    <header className='bg-slate-800 px-5 '>
-      <div className='prose prose-xl mx-auto flex place-items-center justify-between'>
+    <header className='m-5 p-5'>
+      <div className='mx-auto flex place-items-center justify-between'>
         <div className=''>
           <a href='#'>
             <Image
-              src='/images/logo.png'
+              src={`/images/${
+                theme === 'dark'
+                  ? '/svgs/logo-dark.svg'
+                  : '/svgs/logo-light.svg'
+              }`}
               alt='James Drysdale Brand Logo'
               height={68}
               width={68}
@@ -31,7 +37,15 @@ const Header = () => {
           </a>
         </div>
         <section className='flex'>
-          <nav className='flex'>
+          <nav className='flex items-center md:hidden'>
+            <Image
+              src='/images/svgs/menu.svg'
+              alt='mobile menu button'
+              width='24'
+              height='24'
+            />
+          </nav>
+          <nav className='hidden md:flex'>
             {routes.map((route) => (
               <Link href={route.path} key={route.label} className='h-full'>
                 <p
@@ -44,8 +58,8 @@ const Header = () => {
               </Link>
             ))}
           </nav>
+
           <div className='mx-3.5 flex items-center justify-start'>
-            {/* * */}
             <DarkLightTheme />
           </div>
         </section>
